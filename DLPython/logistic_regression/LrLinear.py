55f7b8a133c417a6263a4eb7796c3f21ed2b86ed
Alexis <alexis.alulema@gmail.com>
2017-06-16 09:20:41 -0500

    Small changes

--- DLPython/logistic_regression/LrLinear.py
+++ DLPython/logistic_regression/LrLinear.py
@@ -1,35 +1,42 @@
-# import numpy as np
-from sklearn import linear_model
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
 
-labels = [[0, 0],
-          [1, 1],
-          [2, 2],
-          [3, 6],
-          [4, 4],
-          [5, 5],
-          [6, 6],
-          [7, 9],
-          [8, 8],
-          [9, 12],
-          [10, 10],
-          [11, 11],
-          [12, 12],
-          [13, 17],
-          [14, 14],
-          [15, 15],
-          [16, 16],
-          [17, 17]]
-targets = [0, 1, 2, 3, 4, 5, 6, 7, 7, 9, 9, 11, 11, 13, 15, 15, 16, 17]
+# Load the diabetes dataset
+diabetes = datasets.load_diabetes()
 
-plt.clf()
-plt.plot(labels, targets, 'b.', markersize=6)
-plt.plot(labels, y_, 'g.-', markersize=6)
 
-reg = linear_model.LinearRegression()
-print(reg.fit(labels, targets))
+# Use only one feature
+diabetes_X = diabetes.data[:, np.newaxis, 2]
 
-X_test = [[0, 1], [1, 2], [2, 2]]
-predictions = reg.predict(X_test)
+# Split the data into training/testing sets
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
 
-print(reg.coef_)
+# Split the targets into training/testing sets
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test = diabetes.target[-20:]
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+
+# Train the model using the training sets
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+# The mean squared error
+print("Mean squared error: %.2f"
+      % np.mean((regr.predict(diabetes_X_test) - diabetes_y_test) ** 2))
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % regr.score(diabetes_X_test, diabetes_y_test))
+
+# Plot outputs
+plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
+plt.plot(diabetes_X_test, regr.predict(diabetes_X_test), color='blue',
+         linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
