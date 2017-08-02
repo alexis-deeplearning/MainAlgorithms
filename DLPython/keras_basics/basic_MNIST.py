15795a49a9bf18bf5502bd6e01a2c8bb710ad128
Alexis <alexis.alulema@gmail.com>
2017-08-02 15:12:37 -0500

    Compiling simple MNIST model

--- DLPython/keras_basics/basic_MNIST.py
+++ DLPython/keras_basics/basic_MNIST.py
@@ -6,16 +6,16 @@ from keras.layers.core import Dense, Activation
 from keras.optimizers import SGD
 from keras.utils import np_utils
 
-np.random.seed(1671) # for reproducibility
+np.random.seed(1671)  # for reproducibility
 
 # Network and training
 NB_EPOCH = 200
 BATCH_SIZE = 128
 VERBOSE = 1
-NB_CLASSES = 10 # number of outputs = number of digits
-OPTIMIZER = SGD() # SGD optimizer, explained later in this chapter
+NB_CLASSES = 10  # number of outputs = number of digits
+OPTIMIZER = SGD()  # SGD optimizer, explained later in this chapter
 N_HIDDEN = 128
-VALIDATION_SPLIT = 0.2 # how much TRAIN is reserved for VALIDATION
+VALIDATION_SPLIT = 0.2  # how much TRAIN is reserved for VALIDATION
 
 # Data: shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -38,3 +38,21 @@ print(X_test.shape[0], 'test samples')
 # Convert class vectors  to binary class matrices
 Y_train = np_utils.to_categorical(y_train, NB_CLASSES)
 Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
+
+# 10 classes
+# final stage is softmax
+model = Sequential()
+model.add(Dense(NB_CLASSES, input_shape=(RESHAPED,)))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer=OPTIMIZER, metrics=['accuracy'])
+
+# Model training
+history = model.fit(
+    X_train,
+    Y_train,
+    batch_size=BATCH_SIZE,
+    epochs=NB_EPOCH,
+    verbose=VERBOSE,
+    validation_split=VALIDATION_SPLIT)
