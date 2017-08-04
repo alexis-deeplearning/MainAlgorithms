25cb4241169ede35714e53a27563c9a890fffd56
Alexis <alexis.alulema@gmail.com>
2017-08-04 03:40:35 -0500

    Improving simple MNIST model

--- DLPython/keras_basics/basic_MNIST.py
+++ DLPython/keras_basics/basic_MNIST.py
@@ -1,21 +1,19 @@
-from __future__ import print_function
 import numpy as np
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation
-from keras.optimizers import SGD
+from keras.layers.core import Dense, Activation, Dropout
+from keras.optimizers import SGD, RMSprop, Adam
 from keras.utils import np_utils
 
 np.random.seed(1671)  # for reproducibility
 
 # Network and training
-NB_EPOCH = 200
 BATCH_SIZE = 128
 VERBOSE = 1
 NB_CLASSES = 10  # number of outputs = number of digits
-OPTIMIZER = SGD()  # SGD optimizer, explained later in this chapter
-N_HIDDEN = 128
+N_HIDDEN = 1024
 VALIDATION_SPLIT = 0.2  # how much TRAIN is reserved for VALIDATION
+DROPOUT = 0.3
 
 # Data: shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -41,12 +39,31 @@ Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
 
 # 10 classes
 # final stage is softmax
+# NB_EPOCH = 200
+# OPTIMIZER = SGD()  # SGD optimizer
+# model = Sequential()
+# model.add(Dense(NB_CLASSES, input_shape=(RESHAPED,)))
+# model.add(Activation('softmax'))
+# model.summary()
+
+NB_EPOCH = 25
+OPTIMIZER = Adam()  # SGD optimizer
 model = Sequential()
-model.add(Dense(NB_CLASSES, input_shape=(RESHAPED,)))
+model.add(Dense(N_HIDDEN, input_shape=(RESHAPED,)))
+model.add(Activation('relu'))
+model.add(Dropout(DROPOUT))
+model.add(Dense(N_HIDDEN))
+model.add(Activation('relu'))
+model.add(Dropout(DROPOUT))
+model.add(Dense(NB_CLASSES))
 model.add(Activation('softmax'))
 model.summary()
 
-model.compile(loss='categorical_crossentropy', optimizer=OPTIMIZER, metrics=['accuracy'])
+# Model compiles
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=OPTIMIZER,
+    metrics=['accuracy'])
 
 # Model training
 history = model.fit(
@@ -56,3 +73,8 @@ history = model.fit(
     epochs=NB_EPOCH,
     verbose=VERBOSE,
     validation_split=VALIDATION_SPLIT)
+
+score = model.evaluate(X_test, Y_test, verbose=VERBOSE)
+print()
+print("Test score: ", score[0])
+print("Test accuracy: ", score[1])
